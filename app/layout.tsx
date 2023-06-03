c5ef2a42f166e1495bdf9ff515bfd31f27a6669d
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2023-06-03 23:46:30 +0300

    Create structure. Create components Tag,Button.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
       <body className={inter.className}>{children}</body>
     </html>
   )
---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import { Htag } from "@/components/Htag.tsx/Htag";
+
+export default function About() {
+    return (
+      <div>
+           <Htag tag='h1'>Hellooo</Htag> 
+      </div>
+    )
+  }
