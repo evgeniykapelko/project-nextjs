3d0ddaab5507fc3aa010e45f0b364f383a0df7f6
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2023-06-04 23:33:32 +0300

    Work with components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Hest</Htag>
       <Button appearance='primary' className='test-props'>Button</Button>
+      <Button appearance='ghost' className='test-props'>Button</Button>
+      <P size='l'>Big size</P>
+      <P size='m'>Medium size</P>
+      <P size='s'>Small size</P>
     </>
   )
 }
